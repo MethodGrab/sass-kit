22b439240510d82623873679ca3b766e95733f3a
Nick <methodgr4b@gmail.com>
2016-09-23 19:58:20 +0100

    tests: Check index.scss compiles OK

--- test/index.spec.js
+++ test/index.spec.js
@@ -11,11 +11,11 @@ const fs = Promise.promisifyAll( _fs );
 // Create an AVA test macro function thats asserts
 // 1) No warnings are thrown
 // 2) The compiled CSS matches the expected CSS
-const macro = ( type ) => {
+const individualFileMacro = ( type ) => {
 
 	// :: ( t: AVA instance, file: string ) → null
 	// The actual AVA macro function
-	const _macro = async ( t, file ) => {
+	const _individualFileMacro = async ( t, file ) => {
 		// console.log( `Start '${file}.scss'...` );
 
 		const src      = fixture( `/input/${type}/_${file}.scss` );
@@ -33,22 +33,54 @@ const macro = ( type ) => {
 		// console.log( `Done '${file}.scss'...\n\n` );
 	};
 
-	_macro.title = ( providedTitle, file ) => `${providedTitle}: ${file}`;
+	_individualFileMacro.title = ( providedTitle, file ) => `${providedTitle}: ${file}`;
 
-	return _macro;
+	return _individualFileMacro;
 
 };
 
 
+// :: ( t: AVA instance, file: string ) → null
+// Macro to check a file compiles without errors or warnings
+const compilesSuccessfullyMacro = async ( t, input, expected ) => {
+	let res;
+	let errors = false;
+
+	const src = path.join( __dirname, '../', input );
+
+	try {
+		res = await compile( src );
+	} catch ( err ) {
+		errors = err.message;
+	}
+
+	t.true( errors === false );
+	t.true( res.warnings.length === 0 );
+};
+
+compilesSuccessfullyMacro.title = ( providedTitle, input ) => `compiles without errors or warnings: ${input}`;
+
+
 // :: (  ) → null
 // Programatically create the tests
-const init = async () => {
+const runProgramaticTests = async () => {
 	const functionFiles = await getFiles( 'functions' );
-	const functionMacro = macro( 'functions' );
+	const functionMacro = individualFileMacro( 'functions' );
 
 	functionFiles.forEach( ( file ) => {
 		test( 'function returns an expected value', functionMacro, file );
 	});
 };
 
-init();
+runProgramaticTests();
+
+
+test( 'compiles with zero output: _index.scss', async t => {
+	const src = path.join( __dirname, '../_index.scss' );
+	const res = await compile( src );
+
+	t.true( res.css === '' );
+});
+
+
+test( compilesSuccessfullyMacro, '_index.scss' );
